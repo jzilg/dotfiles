9f3bccf8fc6eedd35342861bd1f5f9ebb02cc64a
jzilg <jzg@dasburo.com>
2025-07-08 19:57:50 +0200

    add autocompletion

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,21 +7,4 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.cursorline = true
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = {
-    "*.lua",
-    "*.json",
-    "*.ts",
-    "*.tsx",
-    "*.js",
-    "*.jsx",
-    "*.css",
-  },
-  callback = function()
-    local save_cursor = vim.fn.getpos(".")
-    vim.cmd([[%s/\s\+$//e]])
-    vim.fn.setpos(".", save_cursor)
-  end,
-})
-
 require("config.lazy")
